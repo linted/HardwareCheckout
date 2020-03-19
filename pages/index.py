8c922afe8ca16c63b837f38b31ed809c5e4da4cc
linted <linted@users.noreply.github.com>
2020-03-19 12:06:25 -0400

    started working on blueprints

--- pages/index.py
+++ pages/index.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python3
 
-from flask import Flask
+from flask import Flask, Blueprint, render_template, session
+from .login import load_logged_in_user
 
+indexBP = Blueprint("index", __name__, "/")
 
+@indexBP.route("/", methods=('GET'))
 def welcomePage():
     """ 
     This is the Function which serves the landing page of the website.
     """
-    return "Hello World!"
+    return render_template("templates/base.html")
+
+@indexBP.before_app_request
+def check_login():
+    return load_logged_in_user()
--- server.py
+++ server.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python3
 import json
-from pages.index import welcomePage
+from pages.index import indexBP
 from pages.login import loginPage
 from pages.checkout import checkout
 from flask import Flask
 
 app = Flask(__name__)
 
-
-welcomePage = app.route("/")(welcomePage)
+app.register_blueprint(indexBP)
 loginPage = app.route("/login")(loginPage)
 checkout = app.route("/checkout/<uid>")(checkout)
 
