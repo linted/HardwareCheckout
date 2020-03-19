613ea3b89bf4fd21744e921d2541ab3a453f4c7f
linted <linted@users.noreply.github.com>
2020-03-18 20:32:52 -0400

    added the base checkout routine

--- server.py
+++ server.py
@@ -2,6 +2,7 @@
 import json
 from pages.index import welcomePage
 from pages.login import loginPage
+from pages.checkout import checkout
 from flask import Flask
 
 app = Flask(__name__)
@@ -9,7 +10,7 @@ app = Flask(__name__)
 
 welcomePage = app.route("/")(welcomePage)
 loginPage = app.route("/login")(loginPage)
-
+checkout = app.route("/checkout/<uid>")(checkout)
 
 if __name__ == "__main__":
     app.run()
