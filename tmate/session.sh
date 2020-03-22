0ba092f6b434e3cb5ee8f3af85b0f4f5499bcceb
linted <linted@users.noreply.github.com>
2020-03-22 03:34:13 -0400

    passing vars

--- tmate/session.sh
+++ tmate/session.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 set -e
-SOCK=/tmp/tmate.sock
+SOCK=/tmp/tmate$1.sock
 
 env -i tmate -S $SOCK new-session -d
 tmate -S $SOCK wait tmate-ready
 WEB=tmate -S $SOCK display -p "#{tmate_web}"
 WEB_RO=tmate -S $SOCK display -p "#{tmate_web_ro}"
 
-USERNAME_VAR=$1
-PASSWORD_VAR=$2
+USERNAME_VAR=User$1
+PASSWORD_VAR=Password$2
 
 python3 <<EOF
 import requests
