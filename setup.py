1a0fc8800cd856b5cce4ad0c7e887bbe75dffa0f
linted <linted@users.noreply.github.com>
2020-03-22 00:52:10 -0400

    Added roles to the setup script

--- setup.py
+++ setup.py
@@ -1,3 +1,15 @@
 #!/usr/bin/env python3
 from HardwareCheckout import db, create_app
+from HardwareCheckout.models import Role
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
 db.create_all(app=create_app())
+
+session = sessionmaker(bind=create_engine('sqlite:///HardwareCheckout/db.sqlite'))
+s = session()
+s.add(Role(name="Human"))
+s.add(Role(name="Device"))
+s.add(Role(name="Admin"))
+
+s.commit()
