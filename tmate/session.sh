aad3e3069d64eaa83f1b49d1abcf5883713f5f3e
linted <linted@users.noreply.github.com>
2020-03-22 02:35:59 -0400

    starting to try and get the devices to call back

--- tmate/session.sh
+++ tmate/session.sh
@@ -4,5 +4,13 @@ SOCK=/tmp/tmate.sock
 
 tmate -S $SOCK new-session -d
 tmate -S $SOCK wait tmate-ready
-tmate -S $SOCK display -p "#{tmate_web}" > /tmp/web
-tmate -S $SOCK display -p "#{tmate_web_ro}" > /tmp/web_ro
+WEB=tmate -S $SOCK display -p "#{tmate_web}"
+WEB_RO=tmate -S $SOCK display -p "#{tmate_web_ro}"
+env>/tmp/test_$1
+python3 <<EOF
+import requests
+session = requests.session()
+session.post("http://virtual.carhackingvillage.com/login", data={'name':$1,'password':$2})
+session.post("http://virtual.carhackingvillage.com/checkin", json={'web':'$WEB','web_ro':'$WEB_RO'})
+
+EOF
