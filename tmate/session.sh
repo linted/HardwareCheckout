f6cb39928cde66c96eafe9ca5f7f2593bd4f1409
linted <linted@users.noreply.github.com>
2020-03-22 16:56:32 -0400

    added logging of writable ssh

--- tmate/session.sh
+++ tmate/session.sh
@@ -4,13 +4,13 @@ SOCK=/tmp/tmate$1.sock
 
 tmate -S $SOCK new-session -d
 tmate -S $SOCK wait tmate-ready
-WEB=$(tmate -S $SOCK display -p "#{tmate_web}")
+WEB=$(tmate -S $SOCK display -p "#{tmate_web}"), $(tmate -S $SOCK display -p "#{tmate_ssh}")
 WEB_RO=$(tmate -S $SOCK display -p "#{tmate_web_ro}")
 
 python3 <<EOF
 import requests
 session = requests.session()
-session.post("http://virtual.carhackingvillage.com/login", data={'name':'device$1','password':'ASubsfas234$1'})
+session.post("http://virtual.carhackingvillage.com/login", data={'name':'device$1','password':'ASubsfas2341'})
 session.post("http://virtual.carhackingvillage.com/checkin", json={'web':'$WEB','web_ro':'$WEB_RO'})
 
 EOF
