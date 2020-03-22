38e9731d3460365a6ba096103809097865afa84d
linted <linted@users.noreply.github.com>
2020-03-22 10:24:12 -0400

    added pip and requests to the install list

--- tmate/install.sh
+++ tmate/install.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-
+apt-get install -y python3-pip
+pip3 install requests
 sudo cp $SCRIPTPATH/session.sh $SCRIPTPATH/session_restart.sh /usr/local/sbin
 sudo cp $SCRIPTPATH/session.target /etc/systemd/system/
 sudo cp $SCRIPTPATH/session@.service /lib/systemd/system/
