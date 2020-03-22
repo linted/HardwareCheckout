197efab68907764c31fd4c0521ff3208a2f93376
linted <linted@users.noreply.github.com>
2020-03-22 03:24:22 -0400

    testing some things out

--- tmate/install.sh
+++ tmate/install.sh
@@ -2,7 +2,8 @@
 SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 sudo cp $SCRIPTPATH/session.sh $SCRIPTPATH/session_restart.sh /usr/local/sbin
-sudo cp $SCRIPTPATH/session@.service $SCRIPTPATH/session.target /etc/systemd/system/
+sudo cp $SCRIPTPATH/session.target /etc/systemd/system/
+sudo cp $SCRIPTPATH/session@.service /lib/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl start session.target
 sudo systemctl enable session.target
--- tmate/session.sh
+++ tmate/session.sh
@@ -2,15 +2,18 @@
 set -e
 SOCK=/tmp/tmate.sock
 
-tmate -S $SOCK new-session -d
+env -i tmate -S $SOCK new-session -d
 tmate -S $SOCK wait tmate-ready
 WEB=tmate -S $SOCK display -p "#{tmate_web}"
 WEB_RO=tmate -S $SOCK display -p "#{tmate_web_ro}"
-env>/tmp/test_$1
+
+USERNAME_VAR=$1
+PASSWORD_VAR=$2
+
 python3 <<EOF
 import requests
 session = requests.session()
-session.post("http://virtual.carhackingvillage.com/login", data={'name':$1,'password':$2})
+session.post("http://virtual.carhackingvillage.com/login", data={'name':${!USERNAME_VAR},'password':${!PASSWORD_VAR}})
 session.post("http://virtual.carhackingvillage.com/checkin", json={'web':'$WEB','web_ro':'$WEB_RO'})
 
 EOF
