a2cbcd30cf43cee036a615b374b02908aa23c735
linted <linted@users.noreply.github.com>
2020-03-22 03:52:27 -0400

    updated session. basically done

--- tmate/session.sh
+++ tmate/session.sh
@@ -7,13 +7,10 @@ tmate -S $SOCK wait tmate-ready
 WEB=tmate -S $SOCK display -p "#{tmate_web}"
 WEB_RO=tmate -S $SOCK display -p "#{tmate_web_ro}"
 
-USERNAME_VAR=User$1
-PASSWORD_VAR=Password$2
-
 python3 <<EOF
 import requests
 session = requests.session()
-session.post("http://virtual.carhackingvillage.com/login", data={'name':${!USERNAME_VAR},'password':${!PASSWORD_VAR}})
+session.post("http://virtual.carhackingvillage.com/login", data={'name':device$1,'password':ASubsfas234$1)
 session.post("http://virtual.carhackingvillage.com/checkin", json={'web':'$WEB','web_ro':'$WEB_RO'})
 
 EOF
