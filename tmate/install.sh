c0e66f39624078ac94ebb041ac1ce4e95471b06a
linted <linted@users.noreply.github.com>
2020-04-04 16:25:35 -0400

    updated sesion.sh and install.sh

--- tmate/install.sh
+++ tmate/install.sh
@@ -1,8 +1,20 @@
 #!/usr/bin/env bash
+
+if [[ $# -ne 1 ]]; then
+    echo "Usage: $0 <URL>"
+fi
+
 SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 apt-get install -y python3-pip
 pip3 install requests
-sudo cp $SCRIPTPATH/session.sh $SCRIPTPATH/session_restart.sh /usr/local/sbin
+
+if [ -f $SCRIPTPATH/session.sh.bak ]; then
+    mv $SCRIPTPATH/session.sh.bak $SCRIPTPATH/session.sh
+fi
+
+sed -i.bak "s|localhost:5000|$1|g" $SCRIPTPATH/session.sh
+
+sudo cp $SCRIPTPATH/{session.sh,session_restart.sh} /usr/local/sbin
 sudo cp $SCRIPTPATH/session.target /etc/systemd/system/
 sudo cp $SCRIPTPATH/session@.service /lib/systemd/system/
 sudo cp $SCRIPTPATH/autologout.sh /etc/profile.d/autologout.sh
