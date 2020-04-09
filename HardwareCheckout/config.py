43215666f5890565e4e15cc70b5e8089df160777
5hu5ky <sekerkiz42@gmail.com>
2020-04-09 10:34:27 -0700

    Database path update
    Fixed issues with sqlite URI
    Made DB an absolute path

--- HardwareCheckout/config.py
+++ HardwareCheckout/config.py
@@ -1,3 +1,5 @@
 #!/usr/bin/env python3
-import os
-db_path = 'sqlite://' + os.path.join(os.path.dirname(os.path.realpath(__file__)), '../../database/db.sqlite')
+#import os
+#db_path = 'sqlite://' + os.path.join(os.path.dirname(os.path.realpath(__file__)), '../../database/db.sqlite')
+xpath = "/opt/database/db.sqlite"
+db_path = 'sqlite:///{}'.format(xpath)
