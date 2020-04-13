a030ca44cb9cf6c379392c37114a5cf925025031
linted <linted@users.noreply.github.com>
2020-04-13 19:03:49 -0400

    added an admin interface that will show all the RW shells and hide the streams

--- HardwareCheckout/main.py
+++ HardwareCheckout/main.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template
 from flask import current_app as app
+from flask_login import current_user
 
 from . import db, create_app
 from .models import DeviceQueue, User
@@ -14,8 +15,15 @@ def index():
 
     :return:
     """
-    results = db.session.query(User.name, DeviceQueue.roUrl).join(User.deviceQueueEntry).all()
-    return render_template('index.html', terminals=results)
+
+    if not current_user.is_anonymous and current_user.has_roles("Admin"):
+        results = db.session.query(User.name, DeviceQueue.webUrl).join(User.deviceQueueEntry).all()
+        show_streams = False
+    else:
+        results = db.session.query(User.name, DeviceQueue.roUrl).join(User.deviceQueueEntry).all()
+        show_streams = True
+
+    return render_template('index.html', terminals=results, show_streams=show_streams)
 
 
 if __name__ == '__main__':
