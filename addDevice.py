ed957c7e26072b70989ca95690096f9822920b4f
5hu5ky <sekerkiz42@gmail.com>
2020-04-14 02:15:09 -0700

    Added multi-device addition capabilitiy
    #device_name,password,device_type
    device0,a1eb226a-daa0-4698-a9fd-3fdb1807c3a7,Nissan
    device1,d39a8aa2-f416-45fb-892a-e9ca173ca9f9,Nissan
    device2,f14d997a-078f-4ce1-813f-9fb91b792ea6,Nissan
    device3,a502d883-e414-4b01-87e7-cb48241ee561,Nissan
    device4,cafc784e-2368-4b21-bc7c-de82da3c274b,Nissan
    device5,b00b5d4b-0f1f-433f-9063-e68d9cd6544b,Nissan

--- addDevice.py
+++ addDevice.py
@@ -1,37 +1,65 @@
 #!/usr/bin/env python3
 from HardwareCheckout import db, create_app
-from HardwareCheckout.models import User, Role, DeviceType
+from HardwareCheckout.models import DeviceQueue, Role, DeviceType
 from HardwareCheckout.config import db_path
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from werkzeug.security import generate_password_hash
 from argparse import ArgumentParser
+import os
 
 parser = ArgumentParser()
-parser.add_argument("username")
-parser.add_argument("password")
-parser.add_argument("type")
+parser.add_argument('-u', '--username', dest='username', required=False)
+parser.add_argument('-p','--password', dest='password', required=False)
+parser.add_argument('-t', '--type', dest='devtype', required=False)
+parser.add_argument('-l', '--list', help='File containing list of users', dest='userlist', required=False)
 args = parser.parse_args()
 # parser.add_argument("Roles", nargs='+')
 
 session = sessionmaker(bind=create_engine(db_path))
 s = session()
 
-device = s.query(Role).filter_by(name="Device").first()
-typeID = s.query(DeviceType).filter_by(name=args.type).first()
-if not typeID:
-    print("Invalid type")
-    exit(1)
 
-s.add(
-    User(
-        name=args.username, 
+def deviceAdd(username, password, devtype):
+    device = s.query(Role).filter_by(name="Device").first()
+    typeID = s.query(DeviceType).filter_by(name=devtype).first()
+
+    if not typeID:
+        print("Invalid type")
+        exit(1)
+    
+    s.add(
+    DeviceQueue(
+        name=username, 
         password=generate_password_hash(
-            args.password, 
+            password, 
             method="pbkdf2:sha256:45000"
         ),
-        roles=[device],
+        state="want-provision",
         type=typeID.id
     )
-)
-s.commit()
+    )
+    s.commit()
+
+
+if args.userlist and (args.username or args.password or args.devtype):
+    print ("You cannot define username, password and device type when you define a list file!!")
+elif args.username and args.password and args.devtype:
+    deviceAdd(args.username,args.password,args.devtype)
+else:
+    if not os.path.isfile(args.userlist):
+        print ("User list {} doesn't exist!".format(args.userlist))
+        exit(1)
+    with open(args.userlist, 'r') as ulist:
+        try:
+            users = list(filter(bool, ulist.read().split('\n')))
+            for user in users:
+                components = user.split(",")
+                if len(components) < 2:
+                    print("Parameter missing in line {}".format(user))
+                    exit(1)
+                else:
+                    deviceAdd(components[0].strip(),components[1].strip(),components[2].strip())
+        except Exception as e:
+            print("Couldn't read {}, {}".format(args.userlist, e))
+            exit(1)
