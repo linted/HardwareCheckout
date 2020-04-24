7ae147e36768dce09b7fcd801f5e17bc27ee5f62
exocron <github@exocron.me>
2020-04-24 19:58:06 -0400

    Updated WebSocket frontend for queue; alerts are working again and device list is updated

--- HardwareCheckout/static/js/queue.js
+++ HardwareCheckout/static/js/queue.js
@@ -17,6 +17,23 @@
 var updater = {
     socket: null,
 
+    escapeAttribute: attr => attr.replace(/&/g, "&amp;").replace(/"/g, "&quot;").replace(/\xA0/g, "&nbsp;"),
+    escapeHTML: html => html.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/\xA0/g, "&nbsp;"),
+
+    renderDeviceInfo: function (device) {
+        return "<li id=\"device_" +
+            updater.escapeAttribute(device.name) + "\">" +
+            updater.escapeHTML(device.name) + "<ul><li>SSH: " +
+            updater.escapeHTML(device.sshAddr) + "</li><li>Web: <a href=\"" +
+            updater.escapeAttribute(device.webUrl) + "\">" +
+            updater.escapeHTML(device.webUrl) + "</a></li></ul></li>";
+    },
+
+    renderDevices: function (devices) {
+        return "<h6>Devices available:</h6><ul>" +
+            devices.map(updater.renderDeviceInfo).join("") + "</ul>";
+    },
+
     start: function() {
         // TODO change to wss
         var url = "ws://" + location.host + "/queue/event";
@@ -25,22 +42,15 @@ var updater = {
             console.log(event);
             msg = JSON.parse(event.data);
             if (msg.type == "new_device") {
+                document.getElementById("devices").lastElementChild.innerHTML += updater.renderDeviceInfo(msg.device);
                 updater.notify("Your device " + msg.device.name + " is ready. You have five minutes to log in before the device becomes available to the next person in the queue.");
-                updater.addConnectionInfo(msg.device); // TODO: create this method
-            }
-            if (msg.type == "all_devices") {
-                updater.removeAll();
-                for (let device of msg.devices) {
-                    updater.addConnectionInfo(device);
-                }
-            }
-            if(msg.error && msg.error == "success" )
-            {
-                if (msg.message == "device_available") {
-                    updater.notify("Your device " + msg.device + " is ready. You have five minutes to log in before the device becomes available to the next person in the queue.");
-                } else if (msg.message == "device_lost") {
+            } else if (msg.type == "all_devices") {
+                document.getElementById("devices").innerHTML = updater.renderDevices(msg.devices);
+            } else if (msg.type == "rm_device") {
+                document.getElementById("device_" + msg.device.name).remove();
+                if (msg.reason == "queue_timeout") {
                     updater.notify("Your login time for device " + msg.device + " has expired. You may re-enter the queue to try again.");
-                } else if (msg.message == "device_reclaimed") {
+                } else if (msg.reason == "normal") {
                     updater.notify("Thank you for participating in the virtual CHV CTF. If you would like more time, you may enter the queue again.");
                 }
             } else if (msg.error) {
@@ -50,60 +60,12 @@ var updater = {
     },
 
     notify: function(msg, connection) {
-        updater.showMessage(connection)
+        // updater.showMessage(connection)
         if ("Notification" in window && Notification.permission === "granted") {
             new Notification(msg);
         } else {
             alert(msg);
         }
-    },
-
-    removeConnectionInfo: function(connection) {
-        device = document.getElementById(connection.device);
-        updater.removeAll(device);
-        list = domDevices.querySelector("ul");
-        if (!list.hasChildNodes())
-        {
-            domDevices.style.display = "none";
-        }
-    },
-
-    removeAll: function(e) {
-        var child = parent.lastElementChild;  
-        while (child) { 
-            updater.removeAll(child);
-            parent.removeChild(child);
-            child = parent.lastElementChild; 
-        } 
-    },
-
-    showConnectionInfo: function(connection) {
-        let domDevices = document.getElementById("devices");
-        let domList = domDevices.querySelector("ul");
-        domList.innerHTML = "";
-        domDevices.style.display = "block";
-        
-        let domSSH = document.createElement("li");
-        domSSH.innerText = "SSH: " + connection.ssh;
-
-        let domWebLink = document.createElement("a");
-        domWebLink.href = connection.webUrl;
-        domWebLink.innerText = connection.webUrl;
-
-        let domWeb = document.createElement("li");
-        domWeb.innerText = "Web: ";
-        domWeb.appendChild(domWebLink);
-
-        let ul = document.createElement("ul");
-        ul.appendChild(domSSH);
-        ul.appendChild(domWeb);
-
-        let li = document.createElement("li");
-        li.id = connection.device;
-        li.innerText = connection.name;
-        li.appendChild(ul);
-
-        domList.appendChild(li);
     }
 };
 
