008da7a9a475a11aad3a3298d693b458b0679d88
linted <linted@users.noreply.github.com>
2020-05-30 01:36:15 -0400

    updated some queue functions for increased speed

--- HardwareCheckout/queue.py
+++ HardwareCheckout/queue.py
@@ -1,7 +1,7 @@
 from sqlalchemy import event
 from tornado.web import authenticated, RequestHandler
 from tornado.websocket import WebSocketClosedError, WebSocketHandler
-from tornado_sqlalchemy import SessionMixin
+from tornado_sqlalchemy import SessionMixin, as_future
 
 from .models import DeviceType, UserQueue, User
 from .webutil import Blueprint, Waiters, UserBaseHandler
@@ -28,6 +28,7 @@ def on_userqueue_insert(mapper, connection, target):
     QueueWSHandler.waiters.broadcast(message)
 
 
+# TODO: find a better way to do this
 # Assign here to avoid cyclical import
 User.assigned_device_callback = on_user_assigned_device
 
@@ -40,18 +41,20 @@ class QueueWSHandler(UserBaseHandler, WebSocketHandler):
         # Non-None enables compression with default options.
         return {}
 
-    def open(self):
+    async def open(self):
         if self.current_user:
-            QueueWSHandler.waiters[self.current_user.id].add(self)
+            self.waiters[self.current_user.id].add(self)
             # send all devices, in case WS connecton was terminated then re-established
             # and a device was assigned in the meantime
-            devices = self.current_user.get_owned_devices(self.session)
-            devices = [{'name': a[0], 'sshAddr': a[1], 'webUrl': a[2]} for a in devices]
-            self.write_message({'type': 'all_devices', 'devices': devices})
+            with self.make_session() as session:
+                devices = await self.current_user.get_owned_devices_async(session)
+                devices = [{'name': a[0], 'sshAddr': a[1], 'webUrl': a[2]} for a in devices]
+                self.write_message({'type': 'all_devices', 'devices': devices})
         else:
             # support updating queue numbers even if not logged in
-            QueueWSHandler.waiters[-1].add(self)
+            self.waiters[-1].add(self)
 
+    # TODO: find out when this runs and how to make it async
     def on_sent(self, message):
         """Callback for webutil.Waiters"""
         try:
@@ -80,21 +83,36 @@ class ListAllQueuesHandler(SessionMixin, RequestHandler):
 @queue.route(r'/(\d+)')
 class SingleQueueHandler(UserBaseHandler):
     async def get(self, id):
-        id = int(id)
-        # TODO: maybe move this into an async model method
-        self.write({'result': [{'name': name} for name, in self.session.query(User.name).select_from(UserQueue).join(User).filter(UserQueue.type == id).order_by(UserQueue.id)]})
-        self.redirect(self.reverse_url("main"))
+        # TODO: Should this be a restricted function?
+        try:
+            id = int(id)
+        except ValueError:
+            self.render("error.html", error="Invalid Queue")
+
+        with self.make_session() as session:
+            names = await as_future(session.query(User.name).select_from(UserQueue).join(User).filter(UserQueue.type == id).order_by(UserQueue.id).all)
+        self.write({'result': [{'name': name} for name in names]})
+        # self.redirect(self.reverse_url("main"))
 
     @authenticated
     async def post(self, id):
-        id = int(id)
-        entry = self.session.query(UserQueue).filter_by(userId=self.current_user.id, type=id).first()
-        if entry:
-            self.write({'result': 'success'})
-            return
-        entry = UserQueue(userId=self.current_user.id, type=id)
-        self.session.add(entry)
-        self.session.commit()
-        self.current_user.try_to_claim_device(self.session, id)
-        self.write({'result': 'success'})
-        self.redirect(self.reverse_url("main"))
+        try:
+            id = int(id)
+        except ValueError:
+            self.render("error.html", error="Invalid Queue")
+
+        with self.make_session() as session:
+            # Check if the user is already registered for a queue
+            entry = await as_future(session.query(UserQueue).filter_by(userId=self.current_user.id, type=id).first)
+            if entry:
+                self.render("error.html", error="User already registered for this queue")
+
+            # Add user to the queue
+            newEntry = UserQueue(userId=self.current_user.id, type=id)
+            session.add(newEntry)
+
+            # Check if someone is able to claim a device
+            self.current_user.try_to_claim_device(session, id)
+
+            # Send them back to the front page
+            self.redirect(self.reverse_url("main"))
