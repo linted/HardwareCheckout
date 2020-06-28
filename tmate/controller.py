511720bd43abcbd7aaf57fb3f9cd0019d2382b7d
linted <linted@users.noreply.github.com>
2020-06-28 00:15:02 -0400

    More work trying to unify the command stuff

--- tmate/controller.py
+++ tmate/controller.py
@@ -3,8 +3,88 @@ import os
 import re
 import subprocess
 from configparser import ConfigParser
+import asyncore
+from base64 import b64encode
 
 import pyinotify
+from tornado.ioloop import IOLoop, PeriodicCallback
+from tornado import gen
+from tornado.websocket import websocket_connect
+from tornado.escape import json_decode, json_encode
+from tornado.httpclient import HTTPRequest
+
+
+class Client(object):
+    states = ['ready', 'in-queue', 'in-use', 
+            'want-deprovision', 'is-deprovisioned', 
+            'want-provision', 'is-provisioned']
+
+    def __init__(self, url, timeout=300):
+        self.url = url
+        self.timeout = timeout
+        self.ioloop = IOLoop.current()
+        self.ws = None
+
+        # self.ioloop.start()
+
+    def auth_header(self, username, password):
+        return {'Authorization': 'Basic ' + b64encode((username + ':' + password).encode()).decode()}
+
+    async def connect(self, username, password):
+        print("trying to connect")
+        try:
+            self.ws = await websocket_connect(
+                    HTTPRequest(
+                        url=self.url, 
+                        headers=self.auth_header(username, password)
+                        ),
+                    on_message_callback=self.handle_message
+                )
+
+            PeriodicCallback(self.keep_alive, self.timeout * 1000).start()
+        except Exception:
+            print("connection error")
+            self.ws = None
+
+    def keep_alive(self):
+        if self.ws is None:
+            self.connect()
+        else:
+            self.ws.write_message(json_encode({"status":"keep-alive"}))
+
+    async def handle_message(self, message):
+        #TODO make async and fix
+        data = json_decode(message)
+        state = data.get("state", False)
+        if not state or state not in self.states:
+            return
+        elif state == "want-provision":
+            if not self.is_provisioned:
+                if not self.provision():
+                    print("Error: provision.sh returned {}".format(returnCode))
+                    self.ws.write_message(json_encode({'status':'provision-failed'}))
+                    return
+            self.ws.write_message(json_encode({'state':'is-provisioned','ssh':self.ssh,'web':self.web,'web_ro':self.web_ro}))
+        elif state == 'want-deprovision':
+            if self.is_provisioned:
+                if not self.deprovision():
+                    print("Error: deprovision.sh returned {}".format(returnCode))
+                    self.ws.write_message(json_encode({'status':'deprovision-failed'}))
+                    return
+            self.ws.write_message(json_encode({'status':'is-deprovisioned'}))
+        elif state == 'update-expiration':
+            with open(self.profile['timestamp_file'], 'w') as fout:
+                fout.write(str(int(data['expiration'])))
+
+        return            
+
+
+    def deprovision(self):
+        '''
+        TODO: use this to kill a session
+        '''
+        pass
+
 
 
 class New_Session_Handler(pyinotify.ProcessEvent):
@@ -65,3 +145,21 @@ class New_Session_Handler(pyinotify.ProcessEvent):
 
             # TODO: Connect and talk to the server
 
+
+
+def main():
+    if not os.path.exists('/tmp/devices'):
+        os.mkdir('/tmp/devices')
+
+    # Create the wss client
+    c = Client("wss://virtual.carhackingvillage.com") # TODO
+
+    # Create the watcher loop
+    watch_manager = pyinotify.WatchManager()
+    event_notifier = pyinotify.AsyncNotifier(watch_manager, New_Session_Handler())
+
+    for files in os.listdir("/tmp/devices"):
+        if os.path.isdir(files):
+            watch_manager.add_watch(os.path.join("/tmp/devices/", files), pyinotify.IN_CREATE)
+    
+    IOLoop.current().start()
