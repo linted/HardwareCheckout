d672b6ea67c1a1be4f21a2bc75d466ad0ac8f572
linted <linted@users.noreply.github.com>
2020-07-10 09:49:22 -0400

    handle errors if dirs already exist

--- tmate/create_config.py
+++ tmate/create_config.py
@@ -9,13 +9,19 @@ parser.add_argument("prefix", help="device name prefix")
 parser.add_argument("count", type=int)
 args = parser.parse_args()
 
-mkdir("/tmp/devices/")
+try:
+    mkdir("/tmp/devices/")
+except Exception:
+    pass
 
 config = configparser.ConfigParser()
 for i in range(args.count):
     name = "device{}".format(i)
     data_dir = "/tmp/devices/{}".format(name)
-    mkdir(data_dir)
+    try:
+        mkdir(data_dir)
+    except Exception:
+        pass
     config[name] = {
         "username": "{}-{}".format(args.prefix, name),
         "password": uuid4(),
