efeb135527f9eb67f238c7952b6916dca8b4c19e
linted <linted@users.noreply.github.com>
2020-07-11 02:37:16 -0400

    changed how configs are stored

--- tmate/create_config.py
+++ tmate/create_config.py
@@ -2,36 +2,34 @@
 import configparser
 import argparse
 from uuid import uuid4
-from os import mkdir
+from os import mkdir, chmod
+from base64 import b64encode
 
 parser = argparse.ArgumentParser()
 parser.add_argument("prefix", help="device name prefix")
 parser.add_argument("count", type=int)
 args = parser.parse_args()
 
+def gen_password():
+    return uuid4()
+
 try:
     mkdir("/tmp/devices/")
 except Exception:
     pass
 
-config = configparser.ConfigParser()
 for i in range(args.count):
     name = "device{}".format(i)
-    data_dir = "/tmp/devices/{}".format(name)
-    try:
-        mkdir(data_dir)
-    except Exception:
-        pass
-    config[name] = {
-        "username": "{}-{}".format(args.prefix, name),
-        "password": uuid4(),
-        "data_dir": data_dir,
-    }
+    password = gen_password()
+    path = "/root/device{}".format(i)
+    with open(path, 'w') as fout:
+        fout.write("AUTH={}\n".format(b64encode("{}-{}={}".format(args.prefix, name, password))))
+    chmod(path, 0o600)
 
-# TODO better password generation
+config = configparser.ConfigParser()
 config["controller"] = {
     "username": "{}-controller".format(args.prefix),
-    "password": uuid4(),
+    "password": gen_password(),
 }
 
 with open("/opt/hc-client/.config.ini", "w") as confout:
