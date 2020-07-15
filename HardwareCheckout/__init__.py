1b621094a001e66de5f2908bc8495315b207bae1
linted <linted@users.noreply.github.com>
2020-07-14 23:24:08 -0400

    made the pool size unlimited

--- HardwareCheckout/__init__.py
+++ HardwareCheckout/__init__.py
@@ -44,7 +44,7 @@ def create_app():
         cookie_secret=os.environ.get('TORNADO_SECRET_KEY', open(os.path.join(os.path.dirname(os.path.realpath(__file__)), "../cookie.key"),'r').read()),
         template_path="HardwareCheckout/templates/",
         static_path="HardwareCheckout/static/",
-        db=SQLAlchemy(url=db_path),
+        db=SQLAlchemy(url=db_path, engine_options={"pool_size":0}),
         # xsrf_cookies=True,  #TODO
         websocket_ping_interval=10000,
         websocket_ping_timeout=30000,
