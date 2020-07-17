cf2ca76183f4b17befd58c0acbac3435c6623580
Ben Gardiner <ben.l.gardiner@gmail.com>
2020-07-17 02:08:26 +0000

    accept armv7l uname -m type (BBB) as arm

--- tmate/install.sh
+++ tmate/install.sh
@@ -12,7 +12,7 @@ case $(uname -m) in
     i386)   architecture="386" ;;
     i686)   architecture="386" ;;
     x86_64) architecture="amd64" ;;
-    arm)    dpkg --print-architecture | grep -q "arm64" && architecture="arm64" || architecture="arm" ;;
+    arm|armv7l)    dpkg --print-architecture | grep -q "arm64" && architecture="arm64" || architecture="arm" ;;
 esac
 
 
