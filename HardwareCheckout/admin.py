e2d892d0801b417973dcd89928b564b864c502a1
linted <linted@users.noreply.github.com>
2020-07-21 23:50:36 -0400

    started work on getting the add device stuff working

--- HardwareCheckout/admin.py
+++ HardwareCheckout/admin.py
@@ -1,5 +1,5 @@
 
-from tornado.web import authenticated
+from tornado.web import authenticated, MissingArgumentError
 
 from .models import DeviceQueue, Role, DeviceType
 from .webutil import Blueprint, UserBaseHandler
@@ -14,4 +14,18 @@ class AdminHandler(UserBaseHandler):
 
     @authenticated
     async def post(self):
-        pass
+        # Try and get the type parameter so we can decide what type of request this is
+        try:
+            req_type = self.get_argument("type")
+        except MissingArgumentError:
+            return self.render("admin.html", messages="Error in form submission")
+
+        if req_type == "addDeviceType":
+            try:
+                name = self.get_argument("name")
+            except MissingArgumentError:
+                return self.render("admin.html", messages="Missing device type")
+            with self.make_session() as session:
+                session.add(DeviceType(name=name))
+                session.commit()
+                
