05aedb64009dd89edb03811f53999d5f6ac467d7
linted <linted@users.noreply.github.com>
2020-07-24 20:43:57 -0400

    added an addDevice functionality to admin interface

--- HardwareCheckout/admin.py
+++ HardwareCheckout/admin.py
@@ -1,5 +1,8 @@
+from configparser import ConfigParser
 
 from tornado.web import authenticated, MissingArgumentError
+from werkzeug.security import generate_password_hash
+from tornado_sqlalchemy import as_future
 
 from .models import DeviceQueue, Role, DeviceType
 from .webutil import Blueprint, UserBaseHandler
@@ -28,4 +31,39 @@ class AdminHandler(UserBaseHandler):
             with self.make_session() as session:
                 session.add(DeviceType(name=name))
                 session.commit()
-                
+            return
+        elif req_type == "addDevice":
+            try:
+                file = self.request.files['config'][0]
+                device_type = self.get_argument("type")
+            except KeyError:
+                return self.render("admin.html", messages="Missing device config file")
+
+            config = ConfigParser()
+            try:
+                config.read_string(file['body'])
+            except KeyError:
+                return self.render("admin.html", messages="Error while trying to read the file")
+            
+            error_msg = ''
+            with self.make_session() as session:
+                device_type_id = await as_future(session.query(DeviceType.id).filter_by(name=device_type).one)[0]
+
+                for section in config.sections():
+                    try:
+                        session.add(
+                            DeviceQueue(
+                                name=config[section]['username'],
+                                password=generate_password_hash(
+                                    config[section]["password"], 
+                                    method="pbkdf2:sha256:45000"
+                                ),
+                                state="want-provision",
+                                type=device_type_id
+                            )
+                        )
+                    except Exception as e:
+                        error_msg += str(e) + '\n'
+                        continue
+            
+            return self.render("admin.html", messages=error_msg)
