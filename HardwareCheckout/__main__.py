be1d9fb7a083a962e541e7154861526087f0f029
5hu5ky <info@derivativetech.com>
2020-07-26 14:18:24 -0700

    Removing #122

--- HardwareCheckout/__main__.py
+++ HardwareCheckout/__main__.py
@@ -7,33 +7,21 @@ from .config import ssl_config
 tornado.options.parse_command_line()
 if not ssl_config['certfile'] or not ssl_config['keyfile']:
     app = create_app()
-    httpApp.listen(8080)
-    '''
-    http_server = tornado.httpserver.HTTPServer(app)
-    http_server.bind(8080)
-    http_server.start(0)  # forks one process per cpu
-    '''
+    app.listen(8080)
     tornado.ioloop.IOLoop.current().start()
 
 else:
     httpApp = create_redirect()
     redirect_server = tornado.httpserver.HTTPServer(httpApp)
     redirect_server.listen(80)
-    '''
-    redirect_server.bind(80)
-    redirect_server.start(0)  # forks one process per cpu
-    '''
-    
+
     app = create_app()
-    https_server = tornado.httpserver.HTTPServer(app, ssl_options={
+    http_server = tornado.httpserver.HTTPServer(app, ssl_options={
         "certfile": ssl_config['certfile'],
         "keyfile": ssl_config['keyfile'],
         })
-    https_server.listen(443)
-    '''
-    https_server.bind(443)
-    http_server.start(0)  # forks one process per cpu
-    '''
+    http_server.bind(443)
+    http_server.start(0)
     #app.listen(80)
     tornado.ioloop.IOLoop.current().start()
     
