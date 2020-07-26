e88203b46696a9ebbec1cd80d8734cf75e9233c4
5hu5ky <info@derivativetech.com>
2020-07-25 22:24:45 -0700

    Fixing #122
    Added multiprocessing according to Tornado deployment guide.

--- HardwareCheckout/__main__.py
+++ HardwareCheckout/__main__.py
@@ -7,20 +7,27 @@ from .config import ssl_config
 tornado.options.parse_command_line()
 if not ssl_config['certfile'] or not ssl_config['keyfile']:
     app = create_app()
-    app.listen(8080)
+    #httpApp.listen(8080)
+    http_server = tornado.httpserver.HTTPServer(app)
+    http_server.bind(8080)
+    http_server.start(0)  # forks one process per cpu
     tornado.ioloop.IOLoop.current().start()
 
 else:
     httpApp = create_redirect()
     redirect_server = tornado.httpserver.HTTPServer(httpApp)
-    redirect_server.listen(80)
+    #redirect_server.listen(80)
+    redirect_server.bind(80)
+    redirect_server.start(0)  # forks one process per cpu
 
     app = create_app()
-    http_server = tornado.httpserver.HTTPServer(app, ssl_options={
+    https_server = tornado.httpserver.HTTPServer(app, ssl_options={
         "certfile": ssl_config['certfile'],
         "keyfile": ssl_config['keyfile'],
         })
-    http_server.listen(443)
+    #https_server.listen(443)
+    https_server.bind(443)
+    http_server.start(0)  # forks one process per cpu
     #app.listen(80)
     tornado.ioloop.IOLoop.current().start()
     
