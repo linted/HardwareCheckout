338b236d0ae892c90a49826e991c0626de627406
Ben Gardiner <ben.l.gardiner@gmail.com>
2020-08-01 04:19:10 +0000

    setup one villager per session

--- tmate/install.sh
+++ tmate/install.sh
@@ -13,8 +13,16 @@ if test -z "$NUM_SESSIONS"; then
     NUM_SESSIONS=6
 fi
 
+if test -z "$CTF_MODE"; then
+    CTF_MODE="$(which true)"
+fi
+
+UNAMES=""
+for i in $(seq ${NUM_SESSIONS}); do
+    UNAMES="${UNAMES} villager-device$i "
+done
+
 HOSTNAME="$(hostname)"
-UNAME=villager
 APP_PATH=/opt/hc-client
 TMATEURL=https://github.com/tmate-io/tmate/releases/download/2.4.0/
 TMATE64=tmate-2.4.0-static-linux-arm64v8.tar.xz
@@ -41,21 +49,14 @@ if [[ $# -ne 2 ]]; then
     exit 1
 fi
 
-echo "Creating device user $UNAME"
-##Creating a user with a home folder remove -m if you do not want a home folder###
-sudo adduser --disabled-password --gecos "" $UNAME --shell /bin/bash
+prep_user() {
+    UNAME="$1"
 
-###Add villager to dialout group###
-sudo usermod -a -G dialout $UNAME
+    sudo adduser --disabled-password --gecos "" $UNAME --shell /bin/bash
+    sudo usermod -a -G dialout $UNAME
 
-
-#Check if we installed before...
-grep -q '617-440-8667' /home/$UNAME/.bashrc
-REINSTALL=$?
-
-if [[ "$REINSTALL" -eq 1 ]]; then
-
-cat <<"EOF" | sudo -u $UNAME tee -a /home/$UNAME/.bashrc > /dev/null
+    if grep -vq '617-440-8667' /home/$UNAME/.bashrc; then
+        cat <<"EOF" | sudo -u $UNAME tee -a /home/$UNAME/.bashrc > /dev/null
 
 
 echo "
@@ -81,18 +82,46 @@ echo "
   **** PLEASE NOTE - TERMINTATING BASH WILL TERMINATE YOUR SESSION! DON'T CRY LATER!!!! ****
 "
 EOF
+    fi
 
-fi
+    ###Generate a strong ssh key###
+    sudo -u $UNAME test -f /home/$UNAME/.ssh/id_rsa || sudo -u $UNAME ssh-keygen -t rsa -b 4096 -f /home/$UNAME/.ssh/id_rsa -N "" -C "$UNAME@$HOSTNAME"
 
-###Generate a strong ssh key###
-sudo -u $UNAME test -f /home/$UNAME/.ssh/id_rsa || sudo -u $UNAME ssh-keygen -t rsa -b 4096 -f /home/$UNAME/.ssh/id_rsa -N "" -C "$UNAME@$HOSTNAME"
+    sudo -u $UNAME install -m 644 $SCRIPTPATH/.tmate.conf /home/$UNAME/.tmate.conf
+    sudo install -m 755 $SCRIPTPATH/controller.py $APP_PATH/
 
+    if ! grep -q "unset AUTH" /home/$UNAME/.bashrc; then
+    sudo chattr -i /home/$UNAME/.bashrc
+    echo -e "\nunset AUTH\n" | sudo -u $UNAME tee -a /home/$UNAME/.bashrc > /dev/null
+    fi
+
+    if ${CTF_MODE} && ! grep -q "rm -rf" /home/$UNAME/.bashrc; then
+    sudo chattr -i /home/$UNAME/.bashrc
+    echo -e "rm -rf ~/.* ~/* 2>/dev/null\n$(cat /home/$UNAME/.bashrc)" | sudo -u $UNAME tee /home/$UNAME/.bashrc > /dev/null
+    fi
+
+    #Make dead files so villagers can't get code exec on later villagers
+    sudo -u $UNAME touch /home/$UNAME/.dircolors
+    sudo -u $UNAME touch /home/$UNAME/.bash_aliases
+    sudo -u $UNAME touch /home/$UNAME/.bash_profile
+    sudo -u $UNAME touch /home/$UNAME/.bash_login
+    sudo -u $UNAME touch /home/$UNAME/.viminfo
+
+    # Lock down the home dir, make everything immutable
+    sudo chattr +i /home/$UNAME/.* /home/$UNAME/*
+    sudo chattr -i /home /home/$UNAME
+    sudo chattr -R +i /home/$UNAME/.ssh
+
+}
+
+for name in $UNAMES; do
+    prep_user $name
+done
 
 SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 if ! which pip3; then
     sudo apt-get install -y python3-pip
 fi
-
 
 if [[ "${architecture}" == "arm64" ]]; then
 TMATE=$TMATE64
@@ -106,7 +135,6 @@ wget $TMATEURL$TMATE 1>/dev/null
 tar xf $TMATE
 sudo mv $(basename $TMATE .tar.xz)/tmate /usr/bin/
 
-
 # Set-up the virtual environment for controller
 sudo mkdir -p $APP_PATH
 sudo -s -H <<EOF || die "couldn't install python3 virtual env or requirements"
@@ -117,7 +145,6 @@ fi
 $APP_PATH/venv/bin/pip3 install -r $SCRIPTPATH/requirements.txt
 EOF
 
-
 if [ -f $SCRIPTPATH/controller.py.bak ]; then
     mv $SCRIPTPATH/controller.py.bak $SCRIPTPATH/controller.py
 fi
@@ -126,37 +153,6 @@ if [ -f $SCRIPTPATH/.tmate.conf.bak ]; then
 fi
 sed -i.bak "s|localhost:8080|$1|g" $SCRIPTPATH/controller.py
 sed -i.bak "s|localhost:8000|$1|g" $SCRIPTPATH/.tmate.conf
-
-# FIXME: seems like this todo is done
-# TODO: make the fun timer stuff in provision.sh work without needing to run it...
-
-# sudo install -m 755 $SCRIPTPATH/{connected.py,deprovision.sh,device.py,provision.sh} /opt/hc-client
-
-
-sudo -u $UNAME install -m 644 $SCRIPTPATH/.tmate.conf /home/$UNAME/.tmate.conf
-sudo install -m 755 $SCRIPTPATH/controller.py $APP_PATH/
-
-if ! grep -q "unset AUTH" /home/$UNAME/.bashrc; then
-sudo chattr -i /home/$UNAME/.bashrc
-echo -e "\nunset AUTH\n" | sudo -u $UNAME tee -a /home/$UNAME/.bashrc > /dev/null
-fi
-
-if ! grep -q "rm -rf" /home/$UNAME/.bashrc; then
-sudo chattr -i /home/$UNAME/.bashrc
-echo -e "rm -rf ~/.* ~/* 2>/dev/null\n$(cat /home/$UNAME/.bashrc)" | sudo -u $UNAME tee /home/$UNAME/.bashrc > /dev/null
-fi
-
-#Make dead files so villagers can't get code exec on later villagers
-sudo -u $UNAME touch /home/$UNAME/.dircolors
-sudo -u $UNAME touch /home/$UNAME/.bash_aliases
-sudo -u $UNAME touch /home/$UNAME/.bash_profile
-sudo -u $UNAME touch /home/$UNAME/.bash_login
-sudo -u $UNAME touch /home/$UNAME/.viminfo
-
-# Lock down the home dir, make everything immutable
-sudo chattr +i /home/$UNAME/.* /home/$UNAME/*
-sudo chattr -i /home /home/$UNAME
-sudo chattr -R +i /home/$UNAME/.ssh
 
 sudo $SCRIPTPATH/create_config.py $2 "${NUM_SESSIONS}"
 
